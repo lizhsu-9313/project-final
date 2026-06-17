85efb319a9a79fb73a3d3f47b87c0bef2b83fad9
lizhsu-9313 <chia.yingouo@gmail.com>
2026-06-18 03:02:23 +0800

    style update

--- script.js
+++ script.js
@@ -109,7 +109,7 @@
   };
 
   var FORTUNE_IMG_BASE_Y = "93vh";
-  var HAND_BOTTOM_OVERFLOW_PX = 96;
+  var HAND_BOTTOM_OVERFLOW_PX = 106;
 
   var screens = {
     home: document.getElementById("screen-home"),
